a8e3b08fb8b27140009b634a648eaa1d4ea5527a
Sumanthsaadam <saisumanth666@gmail.com>
2020-07-01 14:40:29 +0530

    Updates

---
+++ precache-manifest.7b4860248ca3f50999aaaee69e1f68f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b362bfd986033f4ce6b77fa2e0c453b",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "ad0137db3f316ac0e0bd",
+    "url": "/my-app/static/css/main.a6244dc6.chunk.css"
+  },
+  {
+    "revision": "0ac27ccecaca5553eea2",
+    "url": "/my-app/static/js/2.640f7b8d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.640f7b8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad0137db3f316ac0e0bd",
+    "url": "/my-app/static/js/main.6deab85c.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  }
+]);
